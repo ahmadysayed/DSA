2c114b63601df6284d8943062d663eb93b50d13d
ahmadysayed <ahmadysayedmujtaba@gmail.com>
2023-09-11 14:05:15 +0200

    Check the sum of items to find the target

--- PairSum/main.js
+++ PairSum/main.js
@@ -1,7 +1,7 @@
 // Naive
 function hasPairWithSum(arr, sum){
     var len = arr.length;
-    for (let i = 0; i < arr.len-1; i++) {
+    for (let i = 0; i < len-1; i++) {
         for (let j = 0; j < len; j++) {
             if (arr[i] + arr[j] === sum) {
                 return true;
@@ -11,6 +11,8 @@ function hasPairWithSum(arr, sum){
     }
     return false;
 }
+
+// Better
 
 function hasPairWithSum2(arr, sum){
     const mySet = new Set();
