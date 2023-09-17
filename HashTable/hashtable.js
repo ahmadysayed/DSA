d8aaa62db0e6c3411fada1a077a218c2ba6d0440
ahmadysayed <ahmadysayedmujtaba@gmail.com>
2023-09-17 11:52:37 +0200

    keys

--- HashTable/hashtable.js
+++ HashTable/hashtable.js
@@ -33,6 +33,16 @@ class HashTable {
         }
         return undefined;
     }
+
+    keys() {
+        const keysArray = [];
+        for (let i = 0; i < this.data.length; i++){
+            if(this.data[i]) {
+                keysArray.push(this.data[i][0][0])
+            }
+        }
+        return keysArray;
+    }
 }
 
 const myHashTable = new HashTable(2);
