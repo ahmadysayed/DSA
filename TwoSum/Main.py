9d4c209f4b48b75cc564b5a2ca82e60b7d6180e8
ahmadysayed <ahmadysayedmujtaba@gmail.com>
2023-11-12 23:40:17 +0100

    Find the Sum of two number

---
+++ TwoSum/Main.py
@@ -0,0 +1,63 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# You can return the answer in any order.
+
+ 
+
+# Example 1:
+
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+# Example 2:
+
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+# Example 3:
+
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+ 
+
+# Constraints:
+
+# 2 <= nums.length <= 104
+# -109 <= nums[i] <= 109
+# -109 <= target <= 109
+# Only one valid answer exists.
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Create a dictionary to store the elements and their indices
+        num_dict = {}
+
+        for i, num in enumerate(nums):
+            # Calculate the complement needed to reach the target
+            complement = target - num
+
+            # Check if the complement is already in the dictionary
+            if complement in num_dict:
+                # Return the indices of the current number and its complement
+                return [num_dict[complement], i]
+
+            # If the complement is not in the dictionary, add the current number and its index
+            num_dict[num] = i
+
+        # If no solution is found, return an empty list or handle the error as needed
+        return []
+
+# Test cases
+nums1 = [2, 7, 11, 15]
+target1 = 9
+solution = Solution()
+print(solution.twoSum(nums1, target1))  # Output: [0, 1]
+
+nums2 = [3, 2, 4]
+target2 = 6
+print(solution.twoSum(nums2, target2))  # Output: [1, 2]
+
+nums3 = [3, 3]
+target3 = 6
+print(solution.twoSum(nums3, target3))  # Output: [0, 1]    
